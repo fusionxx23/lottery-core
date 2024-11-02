ae81acc247a0e58085d682297950345e4754f693
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-11-01 21:06:22 -0700

    feat: Counter -> Lottery

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+contract Lottery {
     uint256 public number;
-
+    mapping(uint256 => uint256) public lotteries;
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
